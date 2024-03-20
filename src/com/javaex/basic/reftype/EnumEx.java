436e5e3d4b7651e8d99326365b54ed67505c933e
박민준 <pmj9515@daum.net>
2024-03-20 15:54:05 +0900

    First Commit

---
+++ src/com/javaex/basic/reftype/EnumEx.java
@@ -0,0 +1,23 @@
+package com.javaex.basic.reftype;
+
+public class EnumEx {
+	// enum 메서드 테스트
+	
+	public static void main(String[] args) {
+		Week today = Week.WEDESDAY;
+		
+		System.out.println(today);
+		
+		System.out.printf("Today is %s (%d) %n",
+							today.name(),		// 열거 상수의 문자열 표현
+							today.ordinal());	// 열거 상수의 열거 객체 내 순번
+		
+		String strWeek = "FRIDAY";
+		Week obj = Week.valueOf(strWeek);
+		System.out.printf("obj is %s(%d)%n",
+							obj.name(),
+							obj.ordinal());
+
+	}
+
+}
---
+++ src/com/javaex/basic/reftype/Week.java
@@ -0,0 +1,11 @@
+package com.javaex.basic.reftype;
+
+public enum Week {
+	SUNDAY,
+	MONDA,
+	TUESDAY,
+	WEDESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+}
