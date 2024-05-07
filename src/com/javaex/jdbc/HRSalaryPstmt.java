eec4c25ea5f448a1fe5ccbde0cb79b332b62571b
박민준 <pmj9515@daum.net>
2024-05-07 18:08:00 +0900

    24.05.07

--- src/com/javaex/jdbc/HRSalaryPstmt.java
+++ src/com/javaex/jdbc/HRSalaryPstmt.java
@@ -2,51 +2,50 @@ package com.javaex.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Date;
 import java.util.Scanner;
 
-public class HRSalary {
+public class HRSalaryPstmt {
 
 	public static void main(String[] args) {
 		String dburl = "jdbc:oracle:thin:@localhost:1521:xe";
 		String dbuser = "hr";
 		String dbpass = "hr";
-
-		// Connection, Statement, ResultSet
-
+		
 		Connection conn = null;
-		Statement stmt = null;
+		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-
+		
 		Scanner scanner = new Scanner(System.in);
-
+		
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			conn = DriverManager.getConnection(dburl, dbuser, dbpass);
-
+			
 			System.out.print("최소급여 최대급여:");
 			String keyword = scanner.nextLine();
-
-			String[] input = keyword.trim().split(" ");
-
-			stmt = conn.createStatement();
-
-			String sql = "SELECT first_name || ' ' || last_name, salary FROM employees " 
-					+ "WHERE salary BETWEEN "
-					+ input[0] + " AND " + input[1] + " ORDER BY salary ASC";
 			
-			System.out.println("Query:" + sql);
-
-			rs = stmt.executeQuery(sql); // DB Cursor 반환
-
-			// ResultSet 순회
+			String[] input = keyword.trim().split(" ");
+			
+			//	실행 계획 수립
+			String sql = "SELECT first_name || ' ' || last_name, salary FROM employees " +
+					"WHERE salary BETWEEN ? AND ? ORDER BY salary ASC";
+			pstmt = conn.prepareStatement(sql);
+			//	동적 파라미터 바인딩
+			
+			pstmt.setString(1, input[0]);
+			pstmt.setString(2, input[1]);
+			
+			//	쿼리 수행
+			rs = pstmt.executeQuery();
+			
+			//	rs 순회 출력
 			while (rs.next()) {
-				String name = rs.getString(1);
+				String name = rs.getString(1);	
 				int salary = rs.getInt(2);
-
+				
 				System.out.printf("%s\t%d%n", name, salary);
 			}
 		} catch (ClassNotFoundException e) {
@@ -57,23 +56,16 @@ public class HRSalary {
 			e.printStackTrace();
 		} finally {
 			try {
-				rs.close();
+				if (rs != null) rs.close();
+				if (pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
 			} catch (Exception e) {
-
-			}
-			try {
-				stmt.close();
-			} catch (Exception e) {
-
-			}
-			try {
-				conn.close();
-			} catch (Exception e) {
-
+				
 			}
 		}
 		
 		scanner.close();
+
 	}
 
 }
--- src/com/javaex/jdbc/HRsearchEmployeesPstmt.java
+++ src/com/javaex/jdbc/HRsearchEmployeesPstmt.java
@@ -1,24 +1,23 @@
 package com.javaex.jdbc;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Date;
 import java.util.Scanner;
 
-public class HRSalary {
-
+public class HRsearchEmployeesPstmt {
+			
 	public static void main(String[] args) {
 		String dburl = "jdbc:oracle:thin:@localhost:1521:xe";
 		String dbuser = "hr";
 		String dbpass = "hr";
 
-		// Connection, Statement, ResultSet
-
 		Connection conn = null;
-		Statement stmt = null;
+		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 
 		Scanner scanner = new Scanner(System.in);
@@ -27,28 +26,30 @@ public class HRSalary {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			conn = DriverManager.getConnection(dburl, dbuser, dbpass);
 
-			System.out.print("최소급여 최대급여:");
-			String keyword = scanner.nextLine();
+			System.out.print("검색어를 입력하세요:");
+			String keyword = scanner.next();
 
-			String[] input = keyword.trim().split(" ");
-
-			stmt = conn.createStatement();
-
-			String sql = "SELECT first_name || ' ' || last_name, salary FROM employees " 
-					+ "WHERE salary BETWEEN "
-					+ input[0] + " AND " + input[1] + " ORDER BY salary ASC";
+			String sql = "SELECT first_name || ' ' || last_name, email, phone_number, hire_date "
+					+ "FROM employees WHERE lower(first_name) LIKE ? "
+					+ "OR lower(last_name) LIKE ?";
 			
-			System.out.println("Query:" + sql);
+			pstmt = conn.prepareStatement(sql);
 
-			rs = stmt.executeQuery(sql); // DB Cursor 반환
+			pstmt.setString(1, "%" + keyword.toLowerCase() + "%");
+			pstmt.setString(2, "%" + keyword.toLowerCase() + "%");
+			
+			rs = pstmt.executeQuery();
 
 			// ResultSet 순회
 			while (rs.next()) {
 				String name = rs.getString(1);
-				int salary = rs.getInt(2);
+				String email = rs.getString(2);
+				String phoneNumber = rs.getString(3);
+				Date hireDate = rs.getDate(4);
 
-				System.out.printf("%s\t%d%n", name, salary);
+				System.out.printf("%s: %s, %s, %s%n", name, email, phoneNumber, hireDate);
 			}
+
 		} catch (ClassNotFoundException e) {
 			System.err.println("드라이버를 로드하지 못했습니다.");
 			e.printStackTrace();
@@ -57,22 +58,14 @@ public class HRSalary {
 			e.printStackTrace();
 		} finally {
 			try {
-				rs.close();
+				if (rs != null) rs.close();
+				if (pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
 			} catch (Exception e) {
-
-			}
-			try {
-				stmt.close();
-			} catch (Exception e) {
-
-			}
-			try {
-				conn.close();
-			} catch (Exception e) {
-
+				
 			}
 		}
-		
+
 		scanner.close();
 	}
 
